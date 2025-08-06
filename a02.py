f8df60f4e0fc7949a92a04dac31d2e5cecfe3e0c
miyachun <100542608+miyachun@users.noreply.github.com>
2025-08-06 09:56:45 +0800

    Update a02.py

--- a02.py
+++ a02.py
@@ -1,3 +1,14 @@
-print('請輸入:')
-a = input("input:")
-print('你輸入的是:',a)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+if __name__ =="__main__":
+    app.run()
